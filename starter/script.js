f9d658375f04f5d80b387da4969806c62e265e3d
Ngaicode <Mjombakevin376@gmail.com>
2022-02-03 12:03:58 +0300

    quick introduction into DOM manipulation

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,6 @@
-'use strict';
+"use strict";
+
+// we can select html elements with javascript using their respective class and ID names ,or even the html element tags themselves
+// we need to pass a selector as an argument to the queryselector method,this selector is same as the selectors we would use in css
+// the selected element has to be a string
+console.log(document.querySelector(".message").textContent);
